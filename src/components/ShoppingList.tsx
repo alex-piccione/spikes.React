8775dabcfd20376f3818d631469b8bbbf3737ac7
Alex Piccione <Alex.Piccione@redington.co.uk>
2021-01-25 22:22:37 +0000

    Create Square and Board components and click event

---
+++ src/components/ShoppingList.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class ShoppingList extends React.Component {
+    render() { return (
+        <div className="shopping-list">
+            <h2>Shopping list foer [ this.props.name ]</h2>
+            <ul>
+
+            </ul>
+        </div>)    
+    }
+}
+
+// Example usage:11 <ShoppingList />
