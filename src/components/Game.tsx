4314635c236357505c435223600c817e192b4639
Alex Piccione <Alex.Piccione@redington.co.uk>
2021-01-28 22:21:05 +0000

    Add the possibility to return on a previous move.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Board, Move, Value } from "./Board"
 
 interface GameState {
+  step:number
   moves: Move[]
   xIsNext: boolean
 }
@@ -10,6 +11,7 @@ export class Game extends React.Component<any, GameState> {
   constructor(props: any) {
     super(props)
     this.state = {
+      step: 0,
       moves: [{ squares: Array<Value>(9).fill(null)}], // initial empty move
       xIsNext: true,
     }
@@ -18,7 +20,7 @@ export class Game extends React.Component<any, GameState> {
   }
 
   handleClick(i: number) {    
-    const moves = this.state.moves
+    const moves = this.state.moves.slice(0, this.state.step+1) // cut off eventual "future" steps
     const current = moves[moves.length-1]
     const squares = current.squares.slice() // copy of current
 
@@ -26,7 +28,8 @@ export class Game extends React.Component<any, GameState> {
 
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
-        moves: moves.concat( {squares:squares} ), // attach to history
+        step: moves.length,
+        moves: moves.concat( {squares:squares} ), // add to the step history
         xIsNext: !this.state.xIsNext
     })      
   }
@@ -50,13 +53,30 @@ export class Game extends React.Component<any, GameState> {
     }
   }
 
+  jumpTo(step:number) {
+    this.setState({ 
+        step: step, 
+        xIsNext: (step % 2) == 0})
+  }
+
   render() {
     const moves = this.state.moves;
-    const current = moves[moves.length - 1]
+    const current = moves[this.state.step]
     const winner = this.calculateWinner(current.squares)
     let status = winner
       ? `Winner: ${winner}`
       : `Next player: ${this.state.xIsNext ? "X" : "O"}`
+
+    const steps = moves.map( (move, index) => {
+        const text = index ?
+            "Go to move #" + index :
+            "Go to game start"
+        return (
+            <li key={index}>
+                <button onClick={() => this.jumpTo(index)}>{text}</button>
+            </li>
+        )
+    })
 
     return (
       <div className="game">
@@ -65,7 +85,7 @@ export class Game extends React.Component<any, GameState> {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{}</ol>
+          <ol>{steps}</ol>
         </div>
       </div>
     )
