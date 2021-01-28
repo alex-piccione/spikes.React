4d47415b57397fdb9c42034f77f8f4211f7c8905
Alex Piccione <Alex.Piccione@redington.co.uk>
2021-01-28 19:34:17 +0000

    Move Game in its class. Save history of moves.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,66 +1,30 @@
-import React from "react"
-//import {Value, Square} from "./Square"
+import React, { MouseEventHandler } from "react"
 
-interface BoardProps {
+export type Value = null | "X" | "O"
 
+export type Move = {
+  squares: Value[]
 }
 
-interface BoardState {
-  squares:Array<Value>
+interface BoardProps {
+  move: Move,
+  onClick: Function
+}
+
+interface BoardState {  
+  move:Move,
   xIsNext:boolean
 }
 
 export class Board extends React.Component<BoardProps, BoardState> {
 
-  constructor(props:BoardProps) {
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
-
-  handleClick(i:number) {
-      const squares = this.state.squares.slice()
-      squares[i] = this.state.xIsNext ? 'X' : 'O'
-      this.setState({
-          squares: squares,
-          xIsNext: !this.state.xIsNext
-        })      
-  }
-
-  calculateWinner(squares:Array<Value>) {
-    const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]
-
-    for (let i =0; i<lines.length; i++) {
-      const [a,b,c] = lines[i]
-      if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c])
-        return squares[a]
-    }    
-  }
-
   renderSquare (i:number) {
-    return <Square value={this.state.squares[i]} onclick={() => this.handleClick(i)} />
+    return <Square value={this.props.move.squares[i]} onclick={() => this.props.onClick(i)} />
   }
 
   render () {
-    const winner = this.calculateWinner(this.state.squares)
-    let status = winner ? 
-                 `Winner: ${winner}` : 
-                 `Next player: ${this.state.xIsNext?"X":"O"}`
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -82,7 +46,7 @@ export class Board extends React.Component<BoardProps, BoardState> {
 
 }
 
-type Value = null | "X" | "O"
+
 
 interface SquareProps {
   value: Value,
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 
-import {Board} from "./components/Board"
+import {Game} from "./components/Game"
 
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -18,23 +18,6 @@ import {Board} from "./components/Board"
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVit
-
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board></Board>
-        </div>
-        <div className="game-info">
-          <div>{/*status*/}</div>
-          <ol>{}</ol>
-        </div>
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(
   <Game />, document.getElementById("root")
