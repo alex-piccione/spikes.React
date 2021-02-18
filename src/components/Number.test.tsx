2e9db4c5cd2c0866d9e33e024ff85a9a648bce5a
Alex Piccione <Alex.Piccione@redington.co.uk>
2021-02-18 20:35:26 +0000

    Implement test for component style changed based on state change

--- src/components/Number.test.tsx
+++ src/components/Number.test.tsx
@@ -1,7 +1,30 @@
-describe("When clicks", () => {
-  describe("is zero", () => {
-    it("cololr is red", () => {
-      expect("").toBe("red")
-    })
+import { Number } from "./AuthorQuiz"
+
+describe("Number", () => {
+  
+  let number:Number
+  let getColor = () => number.numberStyle().color
+
+  beforeAll(() => {
+    number = new Number({value:0})      
   })
+
+  describe("when clicks", () => {
+
+    describe("is zero", () => {      
+      it("color is red", () => {
+        number.state = {clicks:0}        
+        expect(getColor()).toBe("red")
+      })
+    })
+
+    describe("is one", () => {
+      it("color is blu", () => {
+        number.state = {clicks:1}
+        expect(getColor()).toBe("blue")
+      })
+    })
+
+  })
+
 })
