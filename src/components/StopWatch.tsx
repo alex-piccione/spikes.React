e669972dcf4cfa75e57b8cee2ca7d30aeba62ec5
Alex Piccione <Alex.Piccione@redington.co.uk>
2021-03-29 00:27:04 +0100

    Add Sater, Stop and Reset

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -6,34 +6,51 @@ let model = {
   running: false,
   time: 0
 }
-
-let Intents = {
+/*
+type Intents {
   TICK: "Tick",
   START: "Start",
   STOP: "Stop",
   RESET: "Reset"
-}
+}*/
 
-const view = (model:any) => {
+const view = () => {
   let minutes = Math.floor(model.time / 60)
   let seconds = model.time - (minutes * 60)
   let secondsFormatted = `${seconds < 10 ? '0' : ''}${seconds}`
-  return <div>{minutes}:{secondsFormatted}</div>
+  return <div>
+    <div>{minutes}:{secondsFormatted}</div>
+    <button onClick={handler}>{model.running ? "Stop" : "Start"}</button>
+    <button onClick={reset}>Reset</button>
+  </div>
 }
 
 const render = () => {
-  ReactDOM.render(view(model), document.getElementById("StopWatch"))
+  ReactDOM.render(view(), document.getElementById("StopWatch"))
 }
 
 const update = (model:any, intent:string) => {
     const updates:any = {
-      "TICK": (model:any) => Object.assign(model, {time: model.time + 1 })
+      "TICK": () => Object.assign(model, {time: model.time + 1 }),
+      "START": () => Object.assign(model, {running: true }),
+      "STOP": () => Object.assign(model, {running: false }),
+      "RESET": () => Object.assign(model, {time: 0 }),
     }
     return updates[intent](model)
 }
 
+let handler = (event:any) => {
+  model = update(model, model.running ? "STOP" : "START" )
+  render()
+}
+let reset= () => {
+  model = update(model, "RESET" )
+  render()
+}
+
 setInterval(() => {
-  model = update(model, "TICK")
+  if(model.running)
+    model = update(model, "TICK")
   render()
 }, 1000)
 
