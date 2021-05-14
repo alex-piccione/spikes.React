87ff111c81808b978bdeb0d6f46d475c5daf731e
Alex Piccione <Alex.Piccione@redington.co.uk>
2021-05-14 15:16:16 +0100

    clean up

--- src/components/ImageChangeOnMouseOver.js
+++ src/components/ImageChangeOnMouseOver.js
@@ -1,37 +1,20 @@
-import React, { useRef } from "react"
+import { useRef } from "react"
 
 const ImageChangeOnMouseOver = () => {
 
   const imageRef = useRef(null)
+  const publicUrl = process.env.PUBLIC_URL
+  const [mainImg, overImg] = ["computer_bw.jpg", "computer.jpg"]
 
-  return (
-    <div>
+  return (    
       <img 
         alt="computer"
-        src="/%PUBLIC_URL%/computer.jpg"
-        onMouseOver={() => { imageRef.current.src = "/%PUBLIC_URL%/computer.jpg" }}
-        onMouseOut={() => { imageRef.current.src = "/%PUBLIC_URL%/computer_bw.jpg" }}
+        src={publicUrl + "/" + mainImg} 
+        onMouseOver={() => { imageRef.current.src = publicUrl + "/" + overImg }}
+        onMouseOut={() => { imageRef.current.src = publicUrl + "/" + mainImg }}
         ref={imageRef}
        />
-      <ImageToggleOnMouseOver 
-        primaryImg={process.env.PUBLIC_URL + "/computer.jpg"} 
-        overImg={process.env.PUBLIC_URL + "/computer_bw.jpg"} 
-         alt="" />
-      &nbsp;&nbsp;&nbsp;
-      <ImageToggleOnMouseOver primaryImg="/%PUBLIC_URL%/computer.jpg" overImg="/%PUBLIC_URL%/computer_bw.jpg" alt="computer" />
-    </div>
   )
 }
 
 export default ImageChangeOnMouseOver
-
-
-const ImageToggleOnMouseOver = ({primaryImg, overImg}) => {
-
-  //const imageRef = useRef(null)
-
-  return (
-    <img src={primaryImg} alt="" />
-  )
-
-}
