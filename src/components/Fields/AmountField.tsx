1655862bb1938feb08442c26816767f84ad292ce
Alex <alessandro.piccione.75@gmail.com>
2021-05-16 23:19:33 +0100

    replace useState with useReducer

--- src/components/Fields/AmountField.tsx
+++ src/components/Fields/AmountField.tsx
@@ -1,6 +1,6 @@
 // spike for useReducer Hook
 
-import { useState } from "react"
+import { useReducer, useState } from "react"
 
 interface Props {
   isEditing:boolean,
@@ -9,13 +9,25 @@ interface Props {
 
 const AmountField = (props:Props) => {
   //const [isEditing, setEditing] = useState(false)
-  const [amount, setAmount] = useState(0)
+  //const [amount, setAmount] = useState(0)
+
+  function amountReducer(state:{}, action: {type:string, data: number}) {
+    switch (action.type) {
+      case "amount": {
+        return action.data
+      }
+      default:
+        return state
+    }
+  } 
+
+  const [amount, setAmount] = useReducer(amountReducer, [])
 
   return props.isEditing ?
     (<div className="row">
       <div className="col-auto">      
         <input type="number" className="form-control form-control-sm" defaultValue="0" 
-          onChange={evt => { props.onChange(evt.target.valueAsNumber); setAmount(evt.target.valueAsNumber); }} style={{ display: "inline"}} />
+          onChange={evt => { props.onChange(evt.target.valueAsNumber); setAmount({type:"amount", data: evt.target.valueAsNumber}) /*setAmount(evt.target.valueAsNumber);*/ }} style={{ display: "inline"}} />
       </div>
       <div className="col-auto" style={{ display: "flex", fontSize: "75%"}}>{amount}</div>
       </div>) :
