ac268d509f844d5f08a4d7d0d5f10f1c33672f64
Alex <alessandro.piccione.75@gmail.com>
2021-06-01 22:03:01 +0100

    rename file

--- src/components/Spikes/Spikes.page.tsx
+++ src/components/Spikes/Spikes.page.tsx
@@ -2,7 +2,7 @@ import InputField from "../Fields/InputField"
 import ImageChangeOnMouseOver from "./spike.ImageChangeOnMouseOver"
 import UseEffect from "./spike.UseEffect"
 import ImageToggleOnScroll from "./spike.ImageToggleOnScroll"
-import { Container } from "./spike.child"
+import { Container } from "./spike_child"
 
 const Spikes = () => <>     
   <h1>React Hooks spikes</h1>
